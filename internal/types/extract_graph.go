addf44e77ae7bb475d90ea2a4dfc8a9dbfee8fb2
begoniezhao <begoniezhao@tencent.com>
2025-09-24 12:15:17 +0800

    feat: 新增异步任务提取服务

---
+++ internal/types/extract_graph.go
@@ -0,0 +1,51 @@
+package types
+
+const (
+	TypeChunkExtract = "chunk:extract"
+)
+
+type ExtractChunkPayload struct {
+	TenantID uint   `json:"tenant_id"`
+	ChunkID  string `json:"chunk_id"`
+	ModelID  string `json:"model_id"`
+}
+
+type PromptTemplateStructured struct {
+	Description string      `json:"description"`
+	Examples    []GraphData `json:"examples"`
+}
+
+type GraphNode struct {
+	Name       string            `json:"name,omitempty"`
+	Chunks     []string          `json:"chunks,omitempty"`
+	Attributes map[string]string `json:"attributes,omitempty"`
+}
+
+type GraphRelation struct {
+	Source     *GraphNode        `json:"source,omitempty"`
+	Target     *GraphNode        `json:"target,omitempty"`
+	Type       string            `json:"type,omitempty"`
+	Attributes map[string]string `json:"attributes,omitempty"`
+}
+
+type GraphData struct {
+	Text     string           `json:"text,omitempty"`
+	Node     []*GraphNode     `json:"node,omitempty"`
+	Relation []*GraphRelation `json:"relation,omitempty"`
+}
+
+type NameSpace struct {
+	KnowledgeBase string `json:"knowledge_base"`
+	Knowledge     string `json:"knowledge"`
+}
+
+func (n NameSpace) Labels() []string {
+	res := make([]string, 0)
+	if n.KnowledgeBase != "" {
+		res = append(res, n.KnowledgeBase)
+	}
+	if n.Knowledge != "" {
+		res = append(res, n.Knowledge)
+	}
+	return res
+}
