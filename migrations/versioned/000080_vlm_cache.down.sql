4a2e8c1b626217e30491b304486b864f59b6605d
jw_mac <163wangjack@gmail.com>
2026-07-05 01:05:16 +0800

    feat(cache): VLM OCR/caption result cache with singleflight dedup

---
+++ migrations/versioned/000080_vlm_cache.down.sql
@@ -0,0 +1,5 @@
+-- Migration: 000080_vlm_cache (rollback)
+-- Description: Drop vlm_cache table.
+DO $$ BEGIN RAISE NOTICE '[Migration 000080 DOWN] Dropping vlm_cache table'; END $$;
+DROP TABLE IF EXISTS vlm_cache
+DO $$ BEGIN RAISE NOTICE '[Migration 000080 DOWN] vlm_cache table dropped successfully'; END $$;
