0fc305cb01a1d8b4c04087905ad1da5974edefc3
wizardchen <wizardchen@tencent.com>
2026-09-16 11:28:20 +0800

    feat(website): add product homepage and unified docs deployment

--- website-docs/.vitepress/theme/index.ts
+++ website-docs/.vitepress/theme/index.ts
@@ -1,16 +1,19 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import Landing from './Landing.vue'
+import DefaultTheme from 'vitepress/theme-without-fonts'
+import SiteHeader from './SiteHeader.vue'
 import MermaidZoom from './MermaidZoom.vue'
 import Screenshot from './Screenshot.vue'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
-  Layout: () => h(DefaultTheme.Layout, null, { 'layout-bottom': () => h(MermaidZoom) }),
+  Layout: () => h(DefaultTheme.Layout, null, {
+    'layout-bottom': () => h(MermaidZoom),
+    'layout-top': () => h(SiteHeader),
+    'sidebar-nav-before': () => h('p', { class: 'wk-docs-label' }, '使用文档'),
+  }),
   enhanceApp({ app }) {
-    app.component('Landing', Landing)
     app.component('Screenshot', Screenshot)
   },
 } satisfies Theme
